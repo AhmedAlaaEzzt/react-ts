6aae9b41a45a47d05fa527c50326bbfc96bf7ea2
Ahmed Alaa <ahmed.alaa.ezzt@gmail.com>
2022-04-04 12:10:14 +0200

    00003 - add scroll component to your app

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@
 import { useEffect, useState } from "react";
 import './App.css';
 import CardList from "./components/card-list/card-list";
+import Scroll from "./components/scroll/scroll";
 import UserForm from "./components/user-form/user-form";
 import { IUser } from "./interfaces/User/user";
 function App() {
   const [users, setUsers] = useState<IUser[]>([]);
 
-  const addNewUser = (user: IUser) =>{
+  const addNewUser = (user: IUser) => {
     user.id = users.length + 1;
     setUsers([...users, user]);
   }
@@ -16,13 +17,16 @@ function App() {
     fetch(`https://jsonplaceholder.typicode.com/users`)
       .then((response) => response.json())
       .then((users) => setUsers(users));
-  },[]);
+  }, []);
 
   return (
     <div className="App">
       <h1 className="">Users</h1>
-      <UserForm addNewUser={addNewUser}/>
-      <CardList users={users} />
+      <UserForm addNewUser={addNewUser} />
+      <Scroll>
+        <CardList users={users} />
+      </Scroll>
+
     </div>
   );
 }
