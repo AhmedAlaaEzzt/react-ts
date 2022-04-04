b3c1485100aba4896dff604c4009458b9e413e48
Ahmed Alaa <ahmed.alaa.ezzt@gmail.com>
2022-04-04 10:57:16 +0200

    00002 - change the UserForm componet state to be a user object

--- src/components/user-form/user-form.tsx
+++ src/components/user-form/user-form.tsx
@@ -1,17 +1,16 @@
 import { useState } from "react"
 
 const UserForm =()=>{
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
+    const [user, setUser] = useState({ id: -1, name: "", email: "" });
 
     return <div>
         <label>Name:</label>
-        <input value={name}  onChange={e=> setName(e.target.value)}/>
-        <br/>
+        <input value={user.name} onChange={e => setUser(Object.assign({}, user, { name: e.target.value }))} />
+        <br />
         <label>Email:</label>
-        <input value={email}  onChange={e=> setEmail(e.target.value)}/>
-        <br/>
-        <button>Add New</button>
+        <input value={user.email} onChange={e => setUser(Object.assign({}, user, { email: e.target.value }))} />
+        <br />
+        <button >Add New</button>
     </div>
 }
 
