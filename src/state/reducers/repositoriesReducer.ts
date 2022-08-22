8057c247042e704fd9363c362639ecbf0fe4bb96
Ahmed Alaa <ahmed.alaa.ezzt@gmail.com>
2022-08-22 20:47:14 +0200

    00011 - prepare the demo

--- src/state/reducers/repositoriesReducer.ts
+++ src/state/reducers/repositoriesReducer.ts
@@ -1,28 +1,45 @@
-interface RepositoriesState{
+interface RepositoriesState {
   loading: boolean;
-  error: string | null
-  data: string[],
+  error: string | null;
+  data: string[];
 }
 
-const initialState : RepositoriesState = {
+const initialState: RepositoriesState = {
   loading: false,
   error: null,
   data: [],
 };
 
-interface Action{
-  type: string;
-  payload?: any; // ? means this property is optional
+interface SearchRepositoriesAction {
+  type: "search_repositories";
 }
 
-const reducer = (state : RepositoriesState = initialState, action: Action) : RepositoriesState => {
+interface SearchRepositoriesSuccessAction {
+  type: "search_repositories_success";
+  payload: string[];
+}
+
+interface SearchRepositoriesErrorAction {
+  type: "search_repositories_error";
+  payload: string;
+}
+
+type Action =
+  | SearchRepositoriesAction
+  | SearchRepositoriesSuccessAction
+  | SearchRepositoriesErrorAction;
+
+const reducer = (
+  state: RepositoriesState = initialState,
+  action: Action
+): RepositoriesState => {
   switch (action.type) {
     case "search_repositories":
-        return {...state, error: null, data: [], loading: true}
+      return { ...state, error: null, data: [], loading: true };
     case "search_repositories_success":
-        return {...state, loading: false, error: null, data: action.payload}
+      return { ...state, loading: false, error: null, data: action.payload };
     case "search_repositories_error":
-        return {...state, loading: false, data: [], error: action.payload}
+      return { ...state, loading: false, data: [], error: action.payload };
     default:
       return state;
   }
