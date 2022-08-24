b0f45f7d6dc24998849c42866b75bdd616db2770
Ahmed Alaa <ahmed.alaa.ezzt@gmail.com>
2022-08-24 21:27:56 +0200

    00016 - create onchange and onsubmit for our from

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,7 +1,16 @@
+import { useState } from "react";
+
 const RepositoriesList: React.FC = () => {
+    const [term, setTerm] = useState('');
+
+    const onSubmit =(event : React.FormEvent)=>{
+        event.preventDefault();
+        console.log({term});
+
+    }
     return <div>
-        <form>
-            <input />
+        <form onSubmit={onSubmit}>
+            <input value={term} onChange={e => setTerm(e.target.value)} />
             <button>Search</button>
         </form>
 
