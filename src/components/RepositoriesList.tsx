7db4dbac813c25fbbe950524033f47000a4d4a71
Ahmed Alaa <ahmed.alaa.ezzt@gmail.com>
2022-08-26 17:26:54 +0200

    00020 - prepare the demo

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -7,14 +7,14 @@ import { useActions } from "../Hooks/useActions";
 const RepositoriesList: React.FC = () => {
     const [term, setTerm] = useState('');
     const { searchRepositories } = useActions();
-    
-    const state = useSelector((state: any)=> state.repositories )
-    console.log(state);
+
+    const { error, loading, data } = useSelector((state: any) => state.repositories)
+
 
     const onSubmit = (event: React.FormEvent) => {
         event.preventDefault();
 
-        
+
         searchRepositories(term)
     }
     return <div>
@@ -22,7 +22,9 @@ const RepositoriesList: React.FC = () => {
             <input value={term} onChange={e => setTerm(e.target.value)} />
             <button>Search</button>
         </form>
-
+        {error && <h3>{error}</h3>}
+        {loading && <h3>Loading...</h3>}
+        {!error && !loading && data.map((name) => <div key={name}>{name}</div>)}
     </div>
 }
 
